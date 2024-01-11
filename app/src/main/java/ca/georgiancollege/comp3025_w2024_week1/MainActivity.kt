9ecb8308bcfd6539db97d1b5cb94104b1d9711f6
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2024-01-11 15:39:55 -0500

    initial commit

---
+++ app/src/main/java/ca/georgiancollege/comp3025_w2024_week1/MainActivity.kt
@@ -0,0 +1,17 @@
+package ca.georgiancollege.comp3025_w2024_week1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity()
+{
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val greetingTextView = findViewById<TextView>(R.id.greetingTextView)
+        greetingTextView.text = "Hello, Tom"
+    }
+}
